a3f524afd5abe4d267a7655d484d091a2a3baa82
Veloeli <peter.mosimann@elvetia.ch>
2026-01-25 22:09:39 +0100

    db-check

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('items');
 });
+
+use Illuminate\Support\Facades\DB;
+
+Route::get('/db-check', function () {
+    return [
+        'env' => env('DB_DATABASE'),
+        'config' => config('database.connections.mysql.database'),
+        'actual' => DB::connection()->getDatabaseName(),
+    ];
+});
